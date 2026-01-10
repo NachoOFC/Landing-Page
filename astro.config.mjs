c683941f0a65e20e943177f22ab91b2e58552e6c
NACHO OFC <ignacioperezolavarria@gmail.com>
2026-01-10 18:26:29 -0300

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://github.com/NachoOFC',
-    base: '/Landin-Page',
+    base: '/Landing-Page',
 });
